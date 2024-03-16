bcb1fef24f0b902a0833f4bd389d4ef00d4c413f
naphat-sirisubkulchai <getget01010@gmail.com>
2024-03-17 02:39:11 +0700

    again

---
+++ src/index.ts
@@ -0,0 +1,103 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+
+//เอาทุกอัน
+app.get('/fund', async (req, res) => {
+  const funds = await prisma.fund.findMany()
+  res.json(funds)
+})
+
+//เอาอันที่ไอดีตรง
+app.get('/fund/:unique_id', async (req, res) => {
+  try {
+    const { unique_id } = req.params;
+
+    // Use Prisma to query the database
+    const fund = await prisma.fund.findMany({
+      where: { unique_id},
+    }); 
+
+    if (!fund) {
+      return res.status(404).json({ error: 'Fund not found' });
+    }
+
+    res.json(fund);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+// /filter?searchString={searchString}&take={take}&skip={skip}&orderBy={orderBy}
+app.get('/filter', async (req, res) => {
+  const { searchString, skip, take, orderBy } = req.query
+
+  const or: Prisma.FundWhereInput = searchString
+    ? {
+      OR: [
+        { proj_name_th: { contains: searchString as string } },
+        { proj_name_en: { contains: searchString as string } },
+      ],
+      }
+    : {}
+  const funds = await prisma.fund.findMany({
+    where: {
+      ...or,
+    },
+    take: Number(take) || undefined,
+    skip: Number(skip) || undefined,
+    orderBy: {
+      proj_name_en: orderBy as Prisma.SortOrder,
+    },
+  })
+
+  res.json(funds)
+})
+
+///////////////////////////////// test /////////////////////////////////////////
+//เอาทุกอัน
+app.get('/fund', async (req, res) => {
+  const products = await prisma.product.findMany()
+  res.json(products)
+})
+
+// /filter?searchString={searchString}&take={take}&skip={skip}&orderBy={orderBy}
+app.get('/filter/product', async (req, res) => {
+  const { searchString, skip, take, orderBy } = req.query
+
+  const or: Prisma.ProductWhereInput = searchString
+    ? {
+      OR: [
+        { proj_name_th: { contains: searchString as string } },
+        { proj_name_en: { contains: searchString as string } },
+        { proj_abbr_name: { contains: searchString as string }},
+      ],
+      }
+    : {}
+  const products = await prisma.product.findMany({
+    where: {
+      ...or,
+    },
+    take: Number(take) || undefined,
+    skip: Number(skip) || undefined,
+    orderBy: {
+      proj_name_en: orderBy as Prisma.SortOrder,
+    },
+  })
+
+  res.json(products)
+})
+
+
+
+
+const server = app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
+)
