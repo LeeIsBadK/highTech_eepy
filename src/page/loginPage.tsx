f3e9316b2aa7b976234e4ae552314b3d66ff49d7
best <chisanuchabest@hotmail.com>
2024-03-29 04:14:30 +0700

    update login success

--- src/page/loginPage.tsx
+++ src/page/loginPage.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState, useEffect, useContext } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import lhFund from '../assets/lhFund.png';
 
 import axios from '../loginComponent/api/axios';
